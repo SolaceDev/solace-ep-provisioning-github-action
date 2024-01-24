305b6bf61a7845c43985f1e487c80f25a43bce95
Tamimi <tamimi@solace.com>
2024-01-24 09:31:14 -0800

    build

--- dist/index.js
+++ dist/index.js
@@ -33283,12 +33283,19 @@ try {
     })
   }
 
-
-  shell.exec('cd solace-terraform-provisioning; npm run provision', (code, stderr) => {
-    if (code != 0) {
-      throw new Error(stderr)
-    }
-  })
+  if(PLAN_ONLY != "none") {
+    shell.exec(`cd solace-terraform-provisioning; npm run plan; npm run promote -- -d -appVID ${APPLICATION_VERSION_ID} -mes ${process.env.SOLACE_MESSAGING_SERVICE}`, (code, stderr) => {
+      if (code != 0) {
+        throw new Error(stderr)
+      }
+    })  
+  } else{
+    shell.exec('cd solace-terraform-provisioning; npm run provision', (code, stderr) => {
+      if (code != 0) {
+        throw new Error(stderr)
+      }
+    })
+  } 
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
